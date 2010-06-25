c8e585102abdfd68f06f7b91a0eb125def49d4a6
Simon MacMullen <simon@rabbitmq.com>
2010-06-25 17:29:21 +0100

    Remove the link spam from the sidebar.

--- wordpress-theme/sidebar.php
+++ wordpress-theme/sidebar.php
@@ -53,21 +53,6 @@
 
 			<?php wp_list_categories('show_count=1&title_li=<h2>Categories</h2>'); ?>
 
-			<?php /* If this is the frontpage */ if ( is_home() || is_page() ) { ?>
-				<?php wp_list_bookmarks(); ?>
-
-				<li><h2>Meta</h2>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<li><a href="http://validator.w3.org/check/referer" title="This page validates as XHTML 1.0 Transitional">Valid <abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a></li>
-					<li><a href="http://gmpg.org/xfn/"><abbr title="XHTML Friends Network">XFN</abbr></a></li>
-					<li><a href="http://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress</a></li>
-					<?php wp_meta(); ?>
-				</ul>
-				</li>
-			<?php } ?>
-
 			<?php endif; ?>
 		</ul>
 	</div>
