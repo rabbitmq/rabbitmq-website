deace610e8137c06b6cd22ea86d0ff2525b577c0
Simon MacMullen <simon@rabbitmq.com>
2010-06-25 17:22:55 +0100

    Quick hack-job RabbitMQ theme based on default. Only tested on Firefox.

---
+++ wordpress-theme/footer.php
@@ -0,0 +1,8 @@
+      <div class="clear"></div>
+      <div class="pageFooter"><p><a class="about" href="about.html">About us</a>
+        RabbitMQ&trade; is a Trademark of Rabbit Technologies Ltd.</p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
