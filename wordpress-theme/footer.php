fbdf3b0750db9fc5712a8ee73116ee5e13f86a36
Jim Apperly <jim@rabbitmq.com>
2010-10-06 13:57:00 +0100

    disclaimer restored

--- wordpress-theme/footer.php
+++ wordpress-theme/footer.php
@@ -1,5 +1,8 @@
       <div class="clear"></div>
       <div class="pageFooter">
+      <p><small>The postings on this site are by individual members of the
+          RabbitMQ team, and do not represent VMware’s positions, strategies
+          or opinions.</small></p>
         <p class="righter">
           <a href="contact.html">Contact</a> |
           <a href="about.html">About</a>
