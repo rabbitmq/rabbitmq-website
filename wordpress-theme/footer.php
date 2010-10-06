95429ad629e63a1e1f04402dcf533e29cc7dde8f
Jim Apperly <jim@rabbitmq.com>
2010-10-06 13:39:23 +0100

    added rudimentary contact page

--- wordpress-theme/footer.php
+++ wordpress-theme/footer.php
@@ -1,10 +1,10 @@
       <div class="clear"></div>
       <div class="pageFooter">
-        <p><small>The postings on this site are by individual members of the
-          RabbitMQ team, and do not represent VMware’s positions, strategies
-          or opinions.</small></p>
-        <p><a class="about" href="/about.html">About us</a>
-        RabbitMQ&trade; is a Trademark of Rabbit Technologies Ltd.</p>
+        <p class="righter">
+          <a href="contact.html">Contact</a> |
+          <a href="about.html">About</a>
+        </p>
+        <p>RabbitMQ&#8482; is a Trademark of Rabbit Technologies Ltd.</p>
       </div>
     </div>
   </div>
