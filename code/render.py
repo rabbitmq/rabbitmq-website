efdaf01f7c00d50535ca7f23478dbd1842bb19cf
Simon MacMullen <simon@rabbitmq.com>
2011-06-02 11:54:26 +0100

    So the markdown "codehilite" extension is randomly highlighting pre blocks on some machines here (not sure with what rules). I think we should disable that. Unfortunately the codehilite extension provides no way to disable itself, so we cook one up in a rather ugly way. The also affects the HTML generated and thus the CSS we need.

--- code/render.py
+++ code/render.py
@@ -24,9 +24,14 @@ def preprocess_markdown(fpath):
 
     title = re.search("^#\s*(\S.*\S)\s*$", contents, re.M)
     contents = contents[0:title.start()] + contents[title.end():]
+    title = title.group(1)
 
     entities = open(os.path.join(SITE_DIR, 'rabbit.ent')).read()
     entities = '\n'.join(entities.split('\n')[1:])
+
+    nosyntax = re.search("NOSYNTAX", title)
+    if nosyntax:
+        title = re.sub("NOSYNTAX", "", title)
 
     pre = """<?xml-stylesheet type="text/xml" href="page.xsl"?>
 <!DOCTYPE html PUBLIC "bug in xslt processor requires fake doctype"
@@ -40,12 +45,18 @@ def preprocess_markdown(fpath):
     <title>%s</title>
   </head>
   <body>
-""" % (title.group(1),)
+""" % (title,)
 
     post = """</body>
 </html>
 """
-    processed = markdown.markdown(contents, ["codehilite(css_class=highlight)"])
+    if nosyntax:
+        args = []
+    else:
+        args = ["codehilite(css_class=highlight)"]
+
+    processed = markdown.markdown(contents, args)
+
     # Unfortunately we can't stop markdown escaping entities. Unescape them.
     processed = re.sub(r'&amp;([a-z-]+);', r'&\1;', processed)
 
