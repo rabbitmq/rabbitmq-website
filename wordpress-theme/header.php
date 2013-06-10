a1aaa2f9aed33b8af3c542844726a6ec70e6ae86
Simon MacMullen <simon@rabbitmq.com>
2013-06-10 17:43:08 +0100

    So somehow a708c65529e9 broke the blog. Rather than try to figure out how, let's just revert it. We can live without new pivotal analytics on the blog.

--- wordpress-theme/header.php
+++ wordpress-theme/header.php
@@ -13,7 +13,18 @@
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <script type="text/javascript" src="/js/site.js"/>
-<script type="text/javascript" src="/js/ga-bootstrap.js"/>
+<script xmlns="http://www.w3.org/1999/xhtml" type="text/javascript">
+try{
+ var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-1001800-1']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+}catch(err){};
+      </script>
 </head>
 
 <body>
