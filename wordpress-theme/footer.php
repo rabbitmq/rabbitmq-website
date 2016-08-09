7ae90ac2b1cfca2c0fa17d0e73a6df968fafea2c
Andrew Bruce <me@andrewbruce.net>
2016-08-09 16:04:20 +0100

    Update WordPress theme footer
    [#127911299]

--- wordpress-theme/footer.php
+++ wordpress-theme/footer.php
@@ -3,17 +3,18 @@
       <p><small>The postings on this site are by individual members of the
           RabbitMQ team, and do not represent Pivotal’s positions, strategies
           or opinions.</small></p>
-        <p class="righter">
-          <a href="/sitemap.html">Sitemap</a> |
-          <a href="/contact.html">Contact</a>
-        </p>
-        <p id="copyright">
-          Copyright &#169; 2007-2016 Pivotal Software, Inc. All rights reserved
-          |&#160;<a href="https://pivotal.io/terms-of-use">Terms of Use</a>
-          |&#160;<a href="https://pivotal.io/privacy-policy">Privacy</a>
-          |&#160;<a href="https://github.com/rabbitmq/rabbitmq-website/">This Site is Open Source</a>
-          |&#160;<a href="https://pivotal.io/careers/engineering">Pivotal is Hiring</a>
-        </p>
+      <p>
+        <a href="/sitemap.html">Sitemap</a> |
+        <a href="/contact.html">Contact</a> |
+        <a href="https://github.com/rabbitmq/rabbitmq-website/">This Site is Open Source</a> |
+        <a href="https://pivotal.io/careers/engineering">Pivotal is Hiring</a>
+      </p>
+      <p id="copyright">
+        Copyright &#169; 2007-2016 <a href="https://pivotal.io/">Pivotal Software</a>, Inc. All rights reserved.
+        <a href="https://pivotal.io/terms-of-use">Terms of Use</a>,
+        <a href="https://pivotal.io/privacy-policy">Privacy</a> and
+        <a href="/trademark-guidelines.html">Trademark Guidelines</a>
+      </p>
       </div>
     </div>
   </div>
