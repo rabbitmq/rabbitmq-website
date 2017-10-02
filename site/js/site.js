10a7734154a70745e55353d6a06c446e07a65f20
Michael Klishin <michael@clojurewerkz.org>
2017-10-02 15:17:49 +0300

    Remove GA tracking JS code
    Originally added to track downloads from rabbitmq.com/releases,
    it is no longer necessary: we will be phasing out that path
    in favour of Bintray, Package Cloud and GitHub, each of which offers
    their own stats.
    
    We can add simpler Google Analytics goals that would accompany the
    above stats without this link augmentation magic dust.

--- site/js/site.js
+++ site/js/site.js
@@ -1,56 +1,3 @@
-/*!
- * Javscript snippets for rabbitmq.com.
- *
- */
-
-
-/* A tiny bit of hand-crafted javascript. We could use jquery, but we
-   don't include it at the moment. Requiring 25K more just to
-   make this function prettier is insane.
-
-   The purpose of this function is to decorate all the foreign links on
-   the site to inclue tracking code. We want to know how many times
-   our packages were downloaded, how many times people clicked on our
-   mailto:// and what remote links were popular.
-
-   A bit of google tracking documentation:
-   http://www.google.com/support/analytics/bin/answer.py?hl=en&answer=55529
-   http://code.google.com/apis/analytics/docs/gaJS/gaJSApiBasicConfiguration.html#_gat.GA_Tracker_._trackPageview
-   http://www.google.com/support/googleanalytics/bin/answer.py?hl=en&answer=55527
-
-   In order to achieve that, we iterate through all the links and set
-   "onClick" method to run google tracking code.
-*/
-
-function decorate_links() {
-    /* 'location' should contain something like http://host[:port] */
-    var location = document.location.protocol + "//" + document.location.host;
-    var links = document.getElementsByTagName("a");
-    for(var i=0; i<links.length; i++) {
-        var link = links[i];
-
-        /* When we count a pageview:
-           - When link relates to "/releases", "/debian", "/javadoc"
-           or "/examples" as this directories don't have a tracking
-           code and should be treated as remote content.
-        */
-        if (link.href.match("^" + location + "/(install)/") ) {
-            link.onclick = function () {
-                /* Google analytics don't know how to present remote links,
-                   so we need to pretend they are local. For example:
-                   /remote/http//www.rabbitmq.com/releases/rabbitmq-server/v1.8.0/rabbitmq-server-1.8.0.zip
-                */
-                var url = "/remote/" + this.href.replace("://","/");
-                _gaq.push(['_trackPageview', url]);
-
-                /* Give a tiny bit of time for GA to complete tracking. */
-                setTimeout('document.location = "' + this.href + '"', 100);
-                return false;
-            }
-        }
-    }
-};
-
 /*
  * Handler for the onblur event of the search input text box
  */
@@ -83,13 +30,6 @@ function registerOnLoadHandler(handler) {
 	var fun = window.onload ? window.onload : function() {};
 	window.onload = function() { fun(); handler(); };
 }
-
-registerOnLoadHandler(function() {
-    try{
-        decorate_links();
-    }catch(err){};
-});
-
 
 function onDOMReady(callback) {
     if(document.readyState === "interactive" || document.readyState === "complete") {
