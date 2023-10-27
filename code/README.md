d36685a73c9a9bb56765bfeae9d68fc984069c34
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2023-10-27 16:26:11 +0200

    Install Docusaurus 3.0.0-rc.1

--- code/README.md
+++
@@ -1,6 +0,0 @@
-# Site Generation Code
-
-This directory contains code that renders HTML from the source (DocBook and Markdown).
-If you are looking for site content source, see [./site](../site/) instead.
-
-
--- wordpress-theme/searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-<div>
-<input type="submit" id="searchsubmit" value="Search" />
-<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
-</div>
-</form>
