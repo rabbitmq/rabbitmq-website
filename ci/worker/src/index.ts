11e49f92dc22c9273217e513f661550720852aca
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-11-10 14:39:51 +0100

    Add flow to deploy live

---
+++ ci/worker/src/index.ts
@@ -0,0 +1,27 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+import manifestJSON from '__STATIC_CONTENT_MANIFEST';
+const assetManifest = JSON.parse(manifestJSON);
+
+export default {
+  async fetch(request, env, ctx) {
+    try {
+      // Add logic to decide whether to serve an asset or run your original Worker code
+      return await getAssetFromKV(
+        {
+          request,
+          waitUntil: ctx.waitUntil.bind(ctx),
+        },
+        {
+          ASSET_NAMESPACE: env.__STATIC_CONTENT,
+          ASSET_MANIFEST: assetManifest,
+        }
+      );
+    } catch (e) {
+      let pathname = new URL(request.url).pathname;
+      return new Response(`"${pathname}" not found`, {
+        status: 404,
+        statusText: 'not found',
+      });
+    }
+  },
+};
