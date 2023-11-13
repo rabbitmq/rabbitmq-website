6d7646700b51bba50c02649c752b67a2b2d4967b
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2023-11-13 16:57:56 +0100

    Fix "production" URL in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://www.rabbitmq.com',
+  url: 'https://rabbitmq-website.pages.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
