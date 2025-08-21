52e00ae38f8c24d94c34d4833e19230255bccb80
Michael Klishin <michaelklishin@icloud.com>
2025-08-21 11:45:35 -0400

    Reapply "Add a Licensing page"
    This reverts commit 44afd0af9a0bbf68eda696a1429cb3e21a331555.

---
+++ src/pages/licensing.js
@@ -0,0 +1,117 @@
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+
+export default function Licensing() {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+    <Layout title={`RabbitMQ: ${siteConfig.tagline}`}>
+      <main>
+        <div className={[styles.hero, styles.licensing_hero].join(" ")}>
+          <div className={[styles.container, styles.heroInner].join(" ")}>
+            <Heading as="h1">Licensing</Heading>
+            <div className={styles.licensing_cta}>
+              <Link
+                className="button button--primary"
+                to="mailto:contact-tanzu-data.pdl@broadcom.com"
+              >
+                Contact Us
+              </Link>
+              &nbsp;
+              <Link className="button button--primary" to="/contact#consulting">
+                Find a Partner
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className={styles.services}>
+          <div className={styles.container}>
+            <Heading as="h1">VMware Tanzu RabbitMQ</Heading>
+            <p>
+              With a commercial license, Tanzu RabbitMQ includes features and
+              support beyond the open-source edition - see what’s included:
+            </p>
+            <div className={styles.services_row}>
+              <div className={styles.services_col}>
+                <img src="/img/illu-streaminlined-deployment-and-management.webp" />
+              </div>
+              <div className={styles.services_col}>
+                <Heading as="h3">Longer Support Timelines</Heading>
+                <p>
+                  Extended support lifecycle with critical patches and CVE fixes
+                  for multiple versions.
+                </p>
+              </div>
+              <div className={styles.services_col}>
+                <Heading as="h3">Support from Core Engineers</Heading>
+                <p>
+                  24×7 support from the people who build and maintain RabbitMQ.
+                </p>
+              </div>
+              <div className={styles.services_col}>
+                <img src="/img/bc-vmw-illu-productivity-computer-drkbg.webp" />
+              </div>
+              <div className={styles.services_col}>
+                <img src="/img/illu-enhanced-developer-experience.webp" />
+              </div>
+              <div className={styles.services_col}>
+                <Heading as="h3">Enterprise Security</Heading>
+                <p>
+                  Advanced security including FIPS 140-2 compliance leveraging
+                  OpenSSL3, forward proxy support through OAuth 2.0, and
+                  scanning of RabbitMQ and its dependencies for CVEs.
+                </p>
+              </div>
+              <div className={styles.services_col}>
+                <Heading as="h3">Multi-Data Center Disaster Recovery</Heading>
+                <p>
+                  Efficient schema and data replication to a second data center
+                  supporting promotion of that second site in the event of a
+                  disaster.
+                </p>
+              </div>
+              <div className={styles.services_col}>
+                <img src="/img/illu-robust-security-and-compilance-for-admins.webp" />
+              </div>
+              <div className={styles.services_col}>
+                <img src="/img/bc-vmw-illu-dev-cost-of-migration-drkbg.webp" />
+              </div>
+              <div className={styles.services_col}>
+                <Heading as="h3">AMQP 1.0 over WebSocket</Heading>
+                <p>
+                  Browser-based applications can communicate with RabbitMQ using
+                  AMQP 1.0, making it a practical choice for web-based business
+                  applications.
+                </p>
+              </div>
+              <div className={styles.services_col}>
+                <Heading as="h3">Intra-Cluster Compression</Heading>
+                <p>
+                  In a heavily loaded system with high traffic between RabbitMQ
+                  nodes, compression can reduce the network load by up to 96%,
+                  depending on the nature of the workload.
+                </p>
+              </div>
+              <div className={styles.services_col}>
+                <img src="/img/bc-vmw-illu-productivity-computer-drkbg.webp" />
+              </div>
+              <div className={styles.services_col}></div>
+              <div className={styles.services_col}>
+                <Heading as="h3">Audit Logging</Heading>
+                <p>
+                  VMware Tanzu RabbitMQ on Kubernetes supports audit-logging.
+                  Relevant audit events, for example, which user deleted a
+                  queue, are collected and logged separately.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
