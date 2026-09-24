660bfb5c9365f8f2affe5435f317dd59c439276a
David Ansari <david.ansari@gmx.de>
2026-09-24 12:11:37 +0200

    Remove deprecated Delayed Queues from OSS docs (#2655)

--- src/pages/commercial-features.js
+++ src/pages/commercial-features.js
@@ -88,17 +88,6 @@ export default function CommercialFeatures() {
                 </p>
               </div>
               <div className={styles.services_col}>
-                <Heading as="h3">Delayed Message Support with Replication</Heading>
-                <p>
-                  A special Raft-based replicated queue type that supports delayed message publishing.
-                  This delayed message implementation supports replication and very large backlogs.
-                  {" "}
-                  <Link to="https://techdocs.broadcom.com/us/en/vmware-tanzu/data-solutions/tanzu-rabbitmq-oci/4-2/tanzu-rabbitmq-oci-image/site-delayed-queues.html">
-                    Learn more about Delayed Queues
-                  </Link>.
-                </p>
-              </div>
-              <div className={styles.services_col}>
                 <Heading as="h3">AMQP 1.0 Over WebSockets</Heading>
                 <p>
                   Browser-based applications can communicate with RabbitMQ using
